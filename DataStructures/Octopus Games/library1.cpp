14b2a12b25d79a9da3881ad9f22e727ce1196c22
AvivMaayan <85831013+AvivMaayan@users.noreply.github.com>
2021-12-01 15:51:20 +0200

    library sig

--- DataStructures/Octopus Games/library1.cpp
+++ DataStructures/Octopus Games/library1.cpp
@@ -15,3 +15,72 @@ StatusType AddGroup(void *DS, int GroupID)
     }
     return (StatusType)((Game *)DS)->AddGroup(GroupID);
 }
+
+StatusType AddPlayer(void *DS, int PlayerID, int GroupID, int Level)
+{
+    if (!DS || GroupID <= 0 || PlayerID <= 0, Level<0)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->AddPlayer(PlayerID, GroupID, Level);
+}
+
+StatusType RemovePlayer(void *DS, int PlayerID)
+{
+    if (!DS || PlayerID <= 0)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->RemovePlayer(PlayerID);
+}
+
+StatusType ReplaceGroup(void *DS, int GroupID, int ReplacementID)
+{
+    if (!DS || GroupID <= 0 || ReplacementID <= 0 || GroupID==ReplacementID)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->ReplaceGroup(GroupID, ReplacementID);
+}
+
+StatusType IncreaseLevel(void *DS, int PlayerID, int LevelIncrease)
+{
+    if (!DS || PlayerID <= 0 || LevelIncrease <= 0)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->IncreaseLevel(PlayerID, LevelIncrease);
+}
+
+StatusType GetHighestLevel(void *DS, int GroupID, int *PlayerID)
+{
+    if (!DS || GroupID == 0 || !PlayerID)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->GetHighestLevel(GroupID, PlayerID);
+}
+
+StatusType GetAllPlayersByLevel(void *DS, int GroupID, int **Players, int *numOfPlayers)
+{
+    if (!DS || GroupID == 0 || !Players || !numOfPlayers)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->GetAllPlayersByLevel(GroupID, Players, numOfPlayers);
+}
+
+StatusType GetGroupsHighestLevel(void *DS, int numOfGroups, int **Players)
+{
+    if (!DS || numOfGroups < 1 || !Players)
+    {
+        return (StatusType)INVALID_INPUT;
+    }
+    return (StatusType)((Game *)DS)->GetGroupsHighestLevel(numOfGroups, Players);
+}
+
+void Quit(void** DS)
+{
+    delete *DS;
+    DS = NULL;
+}
